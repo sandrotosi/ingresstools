afff119bdf45b593fcf7cb765939ce197640257f
Sandro Tosi <morph@debian.org>
2016-03-28 14:16:31 +0100

    automate iitc2heatmap.py accepting cli params

--- iitc2heatmap.py
+++ iitc2heatmap.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python3
+
 import json
 import gmplot
+import sys
+import codecs
 
-j = json.load(file('portalData/49k_portalData.json'))
+portalsfile = sys.argv[1]
+city = sys.argv[2]
+
+portaljson = json.load(codecs.open(portalsfile, 'r', 'utf-8-sig'))
 
 # portals in ingress are stored multplied by 10^6
-points = [(x[1]['latE6']/10.0**6, x[1]['lngE6']/10.0**6) for x in j.items()]
+points = [(x['latE6']/10.0**6, x['lngE6']/10.0**6) for x in portaljson['portals']]
 
-# rough filter for portals inside the m25
-p2 = [x for x in points if 51.443052 < x[0] < 51.611683 and -0.291709 < x[1] < 0.077107]
+lats = [x[0] for x in points]
+lngs = [x[1] for x in points]
 
-lats = [x[0] for x in p2]
-lngs = [x[1] for x in p2]
-
-gmap = gmplot.GoogleMapPlotter.from_geocode('London')
+gmap = gmplot.GoogleMapPlotter.from_geocode(city)
 gmap.heatmap(lats, lngs, opacity=0.5)
 
-gmap.draw("london_portals_heatmap.html")
+gmap.draw("%s_portals_heatmap.html" % city.lower())
